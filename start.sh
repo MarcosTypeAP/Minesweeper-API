0c6fb9f73935831501119d31e56a627c2eefe644
MarcosTypeAP <marcosarielpaccor@gmail.com>
2024-01-30 18:03:06 -0300

    add $PORT detection for Render deploy

--- start.sh
+++ start.sh
@@ -8,9 +8,8 @@ if test "$RUN_TESTS"; then
 	python3 -m pytest -p no:cacheprovider --exitfirst || exit 1
 fi
 
-CONTAINER_IP=$(ip -4 -o address | grep -m 1 eth | sed -e "s/^[0-9]\+: \+eth[0-9]\+ \+ inet \+//" -e "s/\/.*//")
-
 if test -z "$FASTAPI_HOST"; then
+	local CONTAINER_IP=$(ip -4 -o address | grep -m 1 eth | sed -e "s/^[0-9]\+: \+eth[0-9]\+ \+ inet \+//" -e "s/\/.*//")
 	export FASTAPI_HOST=$CONTAINER_IP
 fi
 
@@ -21,8 +20,13 @@ if test -z "$(echo $FASTAPI_HOST | grep -E '([0-9]+\.){3}[0-9]+')"; then
 	echo //////////////////////////////////////////////////////
 fi
 
+# Render deploy
+if test -n "$PORT"; then
+	export FASTAPI_PORT=$PORT
+fi
+
 if test "$FASTAPI_DEBUG"; then
-	DEBUG_ARGS=--reload
+	local DEBUG_ARGS=--reload
 fi
 
 python3 -m uvicorn main:app --app-dir src $DEBUG_ARGS --port $FASTAPI_PORT --host $FASTAPI_HOST
